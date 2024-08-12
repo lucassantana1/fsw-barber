22793be9b94b10120758f7088ac067a0b417b4eb
Lucas Santana <lucasdeoliveirasantana874@gmail.com>
2024-08-11 22:03:59 -0300

    feat: add header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
+import Image from "next/image"
 
 const Home = () => {
-  return <Button>Button</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Ola, Lucas!</h2>
+        <p>Domingo, 11 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faca sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
